f60b4d8d4fceec3ab322220730482c82f6c2521c
Ojan Vafai <ojan.vafai@gmail.com>
2019-04-28 16:42:28 +0000

    Get tasks and attachment metadata.

--- background.js
+++ background.js
@@ -1,18 +1,42 @@
-async function fetchData(tabId, options) {
-  chrome.tabs.sendMessage(tabId, options,(response) => {
-    console.log(response);
-  }); 
-}
-
 async function createExport(tabId) {
-  let projects = await fetchData(tabId, {json: "https://app.asana.com/api/1.0/projects"});
+  async function fetchData(options) {
+    return new Promise(resolve => {
+      chrome.tabs.sendMessage(tabId, options,(response) => {
+        resolve(response.data);
+      }); 
+    })
+  };
+
+
+  let projects = await fetchData({json: "https://app.asana.com/api/1.0/projects"});
+
   console.log(projects);
+  let projectData = await Promise.all(projects.map(x=> fetchData(
+    {json: `https://app.asana.com/api/1.0/projects/${x.id}/tasks?opt_pretty&opt_expand=(this%7Csubtasks%2B)`})));
+  
+    console.log(projectData);
+  let attachments = [];
+  await Promise.all(projectData.map(async x=> {
+    await Promise.all(x.map(async task=> {
+      let attachmentData = await fetchData({json: `https://app.asana.com/api/1.0/tasks/${task.id}/attachments`});
+      for (let data of attachmentData) {
+        attachments.push({
+          taskId: task.id,
+          data: data,
+        });
+      }
+    }));
+  }));
+
+  console.log(attachments);
+  // get attachment dats a files
+  // dump projects, projectData, attachments as one big zip
 };
 
 chrome.browserAction.onClicked.addListener(async (extensionTab) => {
   chrome.tabs.create({url: 'https://app.asana.com'}, (asanaTab) => {
     chrome.tabs.executeScript(asanaTab.id,
-      // Need to have asana.com credentials, so do via content script.
+      // Need to have asana.com credentials, so do fetches via content script.
       {code: `
 async function fetchJson(url) {
   let resp = await fetch(url);
@@ -27,19 +51,15 @@ async function fetchText(url) {
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
   if (msg.json) {
     fetchJson(msg.json).then(x=> {
-      console.log(x);
       sendResponse(x);
     });
   } else {
     fetchText(msg.text).then(x=> {
-      console.log(x);
       sendResponse(x);
     });
   }
   return true;
 });
-
-'done';
 `},
     (result) => createExport(asanaTab.id));
   })
