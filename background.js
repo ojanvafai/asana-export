cdb2bb67b905db011e11e32f820a6e8b096c67fb
Ojan Vafai <ojan.vafai@gmail.com>
2021-03-28 15:45:01 -0700

    Fix asana id-->gid and add rate limiting for chrome resource limits.

--- background.js
+++ background.js
@@ -23,6 +23,46 @@ function stringify(obj) {
   });
 }
 
+const MAX_TASKS = 25;
+
+class TaskQueue {
+  constructor() {
+    this.tasks = [];
+    this.inProgressTaskCount = 0;
+    this.resolves = [];
+  }
+
+  doTasks() {
+    if (this.inProgressTaskCount >= MAX_TASKS) return;
+    let task = this.tasks.shift();
+    if (task === undefined) {
+      for (let resolve of this.resolves) resolve();
+      return;
+    }
+    this.inProgressTaskCount++;
+    task().then(() => {
+      this.inProgressTaskCount--;
+      this.doTasks();
+    });
+    this.doTasks();
+  }
+
+  queueTask(task) {
+    const shouldStart = this.tasks.length == 0;
+    this.tasks.push(task);
+    if (shouldStart) this.doTasks();
+  }
+
+  flush() {
+    return new Promise((resolve) => {
+      if (!this.tasks.length && this.inProgressTaskCount == 0) resolve();
+      this.resolves.push(resolve);
+    });
+  }
+}
+
+const taskQueue = new TaskQueue();
+
 async function createExport(tabId) {
   async function sendFetchRequest(options) {
     return new Promise(resolve => {
@@ -39,27 +79,30 @@ async function createExport(tabId) {
   var zip = new JSZip();
   let projectMetadata = await fetchData({json: "https://app.asana.com/api/1.0/projects"});
   let projectData = await Promise.all(projectMetadata.map(x => fetchData(
-    {json: `https://app.asana.com/api/1.0/projects/${x.id}/tasks?opt_pretty&opt_expand=(this%7Csubtasks%2B)`})));
+    {json: `https://app.asana.com/api/1.0/projects/${x.gid}/tasks?opt_pretty&opt_expand=(this%7Csubtasks%2B)`})));
 
   let attachments = [];
-  await Promise.all(projectData.map(async x=> {
-    // TODO: Should probably rate limit this.
-    await Promise.all(x.map(async task=> {
-      let data = await fetchData({json: `https://app.asana.com/api/1.0/tasks/${task.id}/attachments`});
-      for (let attachment of data) {
-        let attachmentMetadata = await fetchData({json: `https://app.asana.com/api/1.0/attachments/${attachment.id}`});
-        // Dropbox download URLs incorrectly have both dl=0 and dl=1 and the dl=0 wins.
-        // Force dl=1 so it gets the dropbox download instead of the viewer.
-        let blobUrl = await sendFetchRequest({blob: attachmentMetadata.download_url.replace('dl=0', 'dl=1')});
+  await Promise.all(projectData.map(async tasks => {
+    tasks.map(task => {
+      taskQueue.queueTask(async () => {
+        let data = await fetchData({json: `https://app.asana.com/api/1.0/tasks/${task.gid}/attachments`});
+        for (let attachment of data) {
+          let attachmentMetadata = await fetchData({json: `https://app.asana.com/api/1.0/attachments/${attachment.gid}`});
+          // Dropbox download URLs incorrectly have both dl=0 and dl=1 and the dl=0 wins.
+          // Force dl=1 so it gets the dropbox download instead of the viewer.
+          let blobUrl = await sendFetchRequest({blob: attachmentMetadata.download_url.replace('dl=0', 'dl=1')});
 
-        zip.folder('attachments').folder(task.id).file(attachment.name, await fetchBlobUrl(blobUrl));
-        attachments.push({
-          taskId: task.id,
-          data: attachmentMetadata,
-        });
-      }
-    }));
+          zip.folder('attachments').folder(task.id).file(attachment.name, await fetchBlobUrl(blobUrl));
+          attachments.push({
+            taskId: task.id,
+            data: attachmentMetadata,
+          });
+        }
+      });
+    });
   }));
+
+  await taskQueue.flush();
 
   zip.file("projects.json", stringify(projectMetadata));
   zip.file("tasks.json", stringify(projectData));
